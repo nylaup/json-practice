7d00938b9b29ccf4036e523aadcc33b407a0dbda
Nylaup <nylaupal@gmail.com>
2024-11-24 19:55:56 -0500

    added files for lab 8

---
+++ lab8/lab8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import json
+output_file = "chacon.csv"
+
+
+with open('../data/schacon.repos.json', 'r') as file:
+    data = json.load(file)
+
+with open(output_file, 'a') as csv_file:
+        csv_file.write("Name,Age\n")
+        for repo in data[:5]:
+                 name = repo.get("name")
+                 html_url = repo.get("html_url")
+                 updated_at = repo.get("updated_at")
+                 visibility = repo.get("visibility")
+                 line = f"{name},{html_url},{updated_at},{visibility}\n"
+                 csv_file.write(line)
